4f0ece1988ae82b1eccaddefcaf79e101d4b032b
强文杰 <672334335@qq.com>
2020-06-15 21:14:19 +0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by 强文杰 on 2020/6/15.
+//  Copyright © 2020 强文杰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
