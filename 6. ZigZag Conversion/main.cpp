c82df740f6c5edbea3bf210b984c91681f27b874
强文杰 <672334335@qq.com>
2020-10-29 21:33:16 +0800

    6

---
+++ 6. ZigZag Conversion/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  6. ZigZag Conversion
+//
+//  Created by 强文杰 on 2020/10/29.
+//  Copyright © 2020 强文杰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
